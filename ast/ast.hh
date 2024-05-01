9b67ea0000eb402805bf975f437049dd68a000e7
wgz_windows <2021201543@ruc.edu.cn>
2024-05-02 00:09:02 +0800

    Lv3.2

--- ast/ast.hh
+++ ast/ast.hh
@@ -65,15 +65,161 @@ public:
 // Stmt 
 class StmtAST : public BaseAST {
 public:
-  int number;
+  std::unique_ptr<BaseAST> exp;
 
   void Dump() const override {
-    std::cout << "StmtAST { " << number << " }";
+    std::cout << "StmtAST { ";
+    exp->Dump();
+    std::cout << " }";
   }
 };
 
-/* // Number
-class Number : public BaseAST {
+// ExpAST
+class ExpAST : public BaseAST {
 public:
-  int int_const;
-}; */
+  std::unique_ptr<BaseAST> add_exp;
+
+  void Dump() const override {
+    std::cout << "ExpAST { ";
+    add_exp->Dump();
+    std::cout << " }";
+  }
+};
+
+// UnaryExpAST 
+class UnaryExpAST : public BaseAST {
+public:
+  enum class Type { PRIMARY, OP } type;
+  std::unique_ptr<BaseAST> son_exp;
+  std::string op;
+
+  UnaryExpAST(std::unique_ptr<BaseAST> &_primary_exp) {
+    type = Type::PRIMARY;
+    son_exp = std::move(_primary_exp);
+  }
+  UnaryExpAST(std::string &_op, std::unique_ptr<BaseAST> &_unary_exp) {
+    type = Type::OP;
+    op = _op;
+    son_exp = std::move(_unary_exp);
+  }
+
+  void Dump() const override {
+    std::cout << "UnaryExpAST { ";
+    switch (type) {
+      case Type::PRIMARY:
+        son_exp->Dump();
+        break;
+      case Type::OP:
+        std::cout << op << ", " ;
+        son_exp->Dump();
+        break;
+      default:
+        break;
+    }
+    std::cout << " }";
+  }
+};
+
+// PrimaryExpAST
+class PrimaryExpAST : public BaseAST {
+public:
+  enum class Type { EXP, NUMBER } type;
+  std::unique_ptr<BaseAST> exp;
+  int number;
+  PrimaryExpAST(std::unique_ptr<BaseAST> &_exp) {
+    type = Type::EXP;
+    exp = std::move(_exp);
+  }
+  PrimaryExpAST(int _number) {
+    type = Type::NUMBER;
+    number = _number;
+  }
+
+  void Dump() const override {
+    std::cout << "PrimaryExpAST { ";
+    switch (type) {
+      case Type::EXP:
+        exp->Dump();
+        break;
+      case Type::NUMBER:
+        std::cout << number;
+      default:
+        break;
+    }
+    std::cout << " }";
+  }
+};
+
+class MulExpAST : public BaseAST {
+public:
+  enum class Type { UNARYEXP, MULEXP} type;
+  std::unique_ptr<BaseAST> unary_exp;
+  std::unique_ptr<BaseAST> mul_exp;
+  std::string op;
+
+  MulExpAST(std::unique_ptr<BaseAST> &_unary_exp) {
+    type = Type::UNARYEXP;
+    unary_exp = std::move(_unary_exp);
+  }
+  MulExpAST(std::unique_ptr<BaseAST> &_mul_exp, std::string &_op, std::unique_ptr<BaseAST> &_unary_exp) {
+    type = Type::MULEXP;
+    mul_exp = std::move(_mul_exp);
+    op = _op;
+    unary_exp = std::move(_unary_exp);
+  }
+
+  void Dump() const override {
+      std::cout << "MulExpAST { ";
+      switch (type)
+      {
+      case Type::UNARYEXP:
+        unary_exp->Dump();
+        break;
+      case Type::MULEXP:
+        mul_exp->Dump();
+        std::cout << ", " << op << ", ";
+        unary_exp->Dump();
+        break;
+      default:
+        break;
+      }
+      std::cout << " }";
+    }
+};
+
+class AddExpAST : public BaseAST {
+public:
+  enum class Type { MULEXP, ADDEXP} type;
+  std::unique_ptr<BaseAST> mul_exp;
+  std::unique_ptr<BaseAST> add_exp;
+  std::string op;
+
+  AddExpAST(std::unique_ptr<BaseAST> &_mul_exp) {
+    type = Type::MULEXP;
+    mul_exp = std::move(_mul_exp);
+  }
+  AddExpAST(std::unique_ptr<BaseAST> &_add_exp, std::string &_op, std::unique_ptr<BaseAST> &_mul_exp) {
+    type = Type::ADDEXP;
+    add_exp = std::move(_add_exp);
+    op = _op;
+    mul_exp = std::move(_mul_exp);
+  }
+
+  void Dump() const override {
+    std::cout << "AddExpAST { ";
+    switch (type)
+    {
+    case Type::MULEXP:
+      mul_exp->Dump();
+      break;
+    case Type::ADDEXP:
+      add_exp->Dump();
+      std::cout << ", " << op << ", ";
+      mul_exp->Dump();
+      break;
+    default:
+      break;
+    }
+    std::cout << " }";
+  }
+};
--- src/sysy.y
+++ src/sysy.y
@@ -24,10 +24,8 @@ using namespace std;
 %parse-param { std::unique_ptr<BaseAST> &ast }
 
 // yylval 的定义, 我们把它定义成了一个联合体 (union)
-// 因为 token 的值有的是字符串指针, 有的是整数
-// 之前我们在 lexer 中用到的 str_val 和 int_val 就是在这里被定义的
-// 至于为什么要用字符串指针而不直接用 string 或者 unique_ptr<string>?
-// 请自行 STFW 在 union 里写一个带析构函数的类会出现什么情况
+// 因为 token 的值有的是字符串指针, 有的是整数，有的是ast指针
+
 %union {
   std::string *str_val;
   int int_val;
@@ -37,20 +35,15 @@ using namespace std;
 // lexer 返回的所有 token 种类的声明
 // 注意 IDENT 和 INT_CONST 会返回 token 的值, 分别对应 str_val 和 int_val
 %token INT RETURN
-%token <str_val> IDENT
+%token <str_val> IDENT UNARYOP ADDOP MULOP
 %token <int_val> INT_CONST
 
 // 非终结符的类型定义
-%type <ast_val> FuncDef FuncType Block Stmt
+%type <ast_val> FuncDef FuncType Block Stmt Exp UnaryExp PrimaryExp MulExp AddExp
 %type <int_val> Number
 
 %%
 
-// 开始符, CompUnit ::= FuncDef, 大括号后声明了解析完成后 parser 要做的事情
-// 之前我们定义了 FuncDef 会返回一个 str_val, 也就是字符串指针
-// 而 parser 一旦解析完 CompUnit, 就说明所有的 token 都被解析了, 即解析结束了
-// 此时我们应该把 FuncDef 返回的结果收集起来, 作为 AST 传给调用 parser 的函数
-// $1 指代规则里第一个符号的返回值, 也就是 FuncDef 的返回值
 CompUnit
   : FuncDef {
     auto comp_unit = make_unique<CompUnitAST>();
@@ -59,16 +52,6 @@ CompUnit
   }
   ;
 
-// FuncDef ::= FuncType IDENT '(' ')' Block;
-// 我们这里可以直接写 '(' 和 ')', 因为之前在 lexer 里已经处理了单个字符的情况
-// 解析完成后, 把这些符号的结果收集起来, 然后拼成一个新的字符串, 作为结果返回
-// $$ 表示非终结符的返回值, 我们可以通过给这个符号赋值的方法来返回结果
-// 你可能会问, FuncType, IDENT 之类的结果已经是字符串指针了
-// 为什么还要用 unique_ptr 接住它们, 然后再解引用, 把它们拼成另一个字符串指针呢
-// 因为所有的字符串指针都是我们 new 出来的, new 出来的内存一定要 delete
-// 否则会发生内存泄漏, 而 unique_ptr 这种智能指针可以自动帮我们 delete
-// 虽然此处你看不出用 unique_ptr 和手动 delete 的区别, 但当我们定义了 AST 之后
-// 这种写法会省下很多内存管理的负担
 FuncDef
   : FuncType IDENT '(' ')' Block {
     auto ast = new FuncDefAST();
@@ -97,9 +80,9 @@ Block
   ;
 
 Stmt
-  : RETURN Number ';' {
+  : RETURN Exp ';' {
     auto ast = new StmtAST();
-    ast->number = $2;
+    ast->exp = unique_ptr<BaseAST>($2);
     $$ = ast;
   }
   ;
@@ -109,6 +92,66 @@ Number
     $$ = $1;
   }
   ;
+
+Exp
+  : AddExp {
+    auto ast = new ExpAST();
+    ast->add_exp = unique_ptr<BaseAST>($1);
+    $$ = ast;
+  }
+  ;
+
+UnaryExp
+  : PrimaryExp {
+    auto primary_exp = unique_ptr<BaseAST>($1);
+    $$ = new UnaryExpAST(primary_exp);
+  }
+  | UNARYOP UnaryExp {
+    auto op = unique_ptr<string>($1);
+    auto unary_exp = unique_ptr<BaseAST>($2);
+    $$ = new UnaryExpAST(*op, unary_exp);
+  }
+  | ADDOP UnaryExp {
+    auto op = unique_ptr<string>($1);
+    auto unary_exp = unique_ptr<BaseAST>($2);
+    $$ = new UnaryExpAST(*op, unary_exp);
+  }
+  ;
+
+PrimaryExp
+  : '(' Exp ')' {
+    auto exp = unique_ptr<BaseAST>($2);
+    $$ = new PrimaryExpAST(exp);
+  }
+  | Number {
+    $$ = new PrimaryExpAST($1);
+  }
+  ;
+
+MulExp
+  : UnaryExp {
+    auto unary_exp = unique_ptr<BaseAST>($1);
+    $$ = new MulExpAST(unary_exp);
+  }
+  | MulExp MULOP UnaryExp {
+    auto mul_exp = unique_ptr<BaseAST>($1);
+    auto op = unique_ptr<string>($2);
+    auto unary_exp = unique_ptr<BaseAST>($3);
+    $$ = new MulExpAST(mul_exp, *op, unary_exp);
+  }
+  ;
+
+AddExp
+  : MulExp {
+    auto mul_exp = unique_ptr<BaseAST>($1);
+    $$ = new AddExpAST(mul_exp);
+  }
+  | AddExp ADDOP MulExp {
+    auto add_exp = unique_ptr<BaseAST>($1);
+    auto op = unique_ptr<string>($2);
+    auto mul_exp = unique_ptr<BaseAST>($3);
+    $$ = new AddExpAST(add_exp, *op, mul_exp);
+  }
 
 %%
 
